c51cb3775c73fec06a93b942314d0d3e4456f378
Ahmed Elsayed <a.e.hamed95@gmail.com>
2024-06-19 16:41:03 +0300

    - ..

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose
   .catch((error) => console.error("MongoDB connection error:", error));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
