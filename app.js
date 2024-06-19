0e85fb7162dd88f9432c1b6ec2e41b9455d33340
Ahmed Elsayed <a.e.hamed95@gmail.com>
2024-06-19 15:31:53 +0300

    - add cors lib

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const agentsRoutes = require("./routes/agentsRoutes");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(bodyParser.json());
 
